d78947100f252f48e1fe131da99993e6e7699315
VoyDcode <Victor_r.lima@hotmail.com>
2025-09-30 21:29:14 -0300

    Iniciando o projeto

---
+++ src/main/java/com/oracle/Nutryon/NutryonApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.Nutryon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutryonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutryonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oracle/Nutryon/NutryonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.Nutryon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutryonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
